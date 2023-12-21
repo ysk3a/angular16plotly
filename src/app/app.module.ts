13ac7848fce48876852cbdfa2e01bf4850f2b326
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-12-21 15:06:09 -0500

    plotly test init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { PlotlyComponent } from './plotly/plotly.component';
+
+// import * as PlotlyJS from 'plotly.js-dist-min';
+// import { PlotlyModule } from 'angular-plotly.js';
+// PlotlyModule.plotlyjs = PlotlyJS;
+import { PlotlyViaWindowModule } from 'angular-plotly.js';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PlotlyComponent
+    ],
+    imports: [
+        BrowserModule,
+        // PlotlyModule,
+        CommonModule,
+        PlotlyViaWindowModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/plotly/plotly.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import * as Plotly from 'plotly.js';
+// import * as Plotly from 'plotly.js';
+import { PlotData } from "plotly.js"; // or whatever you need
+import { Config, Data, Layout } from 'plotly.js-dist-min';
+// plotly.com/javascript/axes/#multi-category-axes
+@Component({
+    selector: 'app-plotly',
+    templateUrl: './plotly.component.html',
+    styleUrls: ['./plotly.component.scss']
+})
+export class PlotlyComponent {
+    public graph = {
+        data: [{ x: [1, 2, 3], y: [2, 5, 3], type: 'bar' }],
+        layout: { autosize: true, title: 'A Fancy Plot' },
+    };
+
+
+    trace1 = {
+        x: [
+            ['SF Zoo', 'SF Zoo', 'SF Zoo'],
+            ['giraffes', 'orangutans', 'monkeys']
+        ],
+        y: [20, 14, 23],
+        name: 'SF Zoo',
+        // type: 'bar'
+        type: 'bar' as Plotly.PlotType,
+    };
+    trace2 = {
+        x: [
+            ['LA Zoo', 'LA Zoo', 'LA Zoo'],
+            ['giraffes', 'orangutans', 'monkeys']
+        ],
+        y: [12, 18, 29],
+        name: 'LA Zoo',
+        type: 'bar' as Plotly.PlotType,
+    };
+    //multicategory
+    data: Partial<PlotData>[] = [this.trace1, this.trace2];
+    layout: Partial<Layout> = {
+        showlegend: false,
+        yaxis: {
+            fixedrange: true,
+        },
+        xaxis: {
+            // tickson: "boundaries",
+            ticklen: 15,
+            showdividers: true,
+            dividercolor: 'grey',
+            dividerwidth: 2,
+            type: 'multicategory',
+            fixedrange: true,
+        },
+        title: 'awesome axis title here'
+    };
+    // Plotly.newPlot('myDiv', data, layout);
+    config: Partial<Config> = {
+        scrollZoom: false,
+        displayModeBar: false,
+        editable: false,
+        showLink: false,
+        displaylogo: false,
+    }
+    // public graph2:
+    constructor() {
+        // Plotly.newPlot('myDiv', this.data, this.layout);
+
+    }
+}
